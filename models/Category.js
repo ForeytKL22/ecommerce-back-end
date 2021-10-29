844dfc2e8ede36741ea8c8d76ece8e215ce04ce8
ForeytKL22 <krforeyt@gmail.com>
2021-10-28 19:35:03 -0600

    added models

--- models/Category.js
+++ models/Category.js
@@ -1,4 +1,3 @@
-
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -7,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
