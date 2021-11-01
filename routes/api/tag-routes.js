6ecadb0f185a6d8923a1c7a064b651381649e66f
ForeytKL22 <krforeyt@gmail.com>
2021-10-31 20:11:49 -0600

    final commit

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,16 +8,15 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   Tag.findAll({
-    //   attributes: [
-    //       'id',
-    //       'tag_name'
-    //   ],
+      attributes: [
+          'id',
+          'tag_name'
+      ],
       // be sure to include its associated Product data
       include: [
           {
               model: Product,
-              through: ProductTag
-            //   attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+              attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
           },
       ],
   })
